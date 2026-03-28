6c78135b6e9993f25f3b82e319741100b49d8814
mytagssma <161827049+mytagssma@users.noreply.github.com>
2026-03-29 00:39:24 +0530

    Update package path for freedownloadmanager

--- flake.nix
+++ flake.nix
@@ -6,6 +6,6 @@
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.freedownloadmanager =
       nixpkgs.legacyPackages.x86_64-linux.callPackage
-        .package.nix { };
+        ./package.nix { };
   };
 }
