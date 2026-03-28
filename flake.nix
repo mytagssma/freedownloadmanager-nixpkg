47f859aae103f0701240eac3120d0711e582e20c
mytagssma <161827049+mytagssma@users.noreply.github.com>
2026-03-29 01:05:59 +0530

    Update flake.nix for package import and configuration

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
   description = "freedownloadmanager-nixpkg";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.freedownloadmanager =
-      nixpkgs.legacyPackages.x86_64-linux.callPackage
-        ./package.nix { };
-  };
+  outputs = { self, nixpkgs }: 
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config = { allowUnfree = true; };
+      };
+    in
+    {
+      packages.x86_64-linux.freedownloadmanager = 
+        pkgs.callPackage ./package.nix { };
+    };
 }
